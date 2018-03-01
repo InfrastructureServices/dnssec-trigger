5059df1b4b522c4e297037933072e2298a2426c5
Martin Sehnoutka <msehnout@redhat.com>
2018-03-01 14:36:08 +0100

    Reimplement python lock in C

---
+++ riggerd/lock.h
@@ -0,0 +1,28 @@
+#include "config.h"
+
+#if !defined LOCK_H && defined FWD_ZONES_SUPPORT
+#define LOCK_H
+
+#define __LOCK_FILE_DIR "/var/run/dnssec-trigger"
+#define __LOCK_FILE_PATH "/var/run/dnssec-trigger/lock"
+
+/** Check lock file presence and acquire the lock. If the file is
+ * already locked, block until it is released.
+ * TODO: possible errors
+ */
+void lock_acquire();
+
+/** 
+ * Release the lock.
+ * TODO: possible errors
+ */
+void lock_release();
+
+/**
+ * Override lock file location. For testing purposes only. The function
+ * stores the pointer, it does not copy the content, so the content must
+ * live as long as the lock is used.
+ */
+void lock_override(char *path, size_t len);
+
+#endif /* LOCK_H */
