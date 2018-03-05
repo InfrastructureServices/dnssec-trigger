6895168c9e7455ae1947670ae9d3aa351c68bacf
Martin Sehnoutka <msehnout@redhat.com>
2018-03-05 12:32:01 +0100

    simple test for the lock module

--- test/other.c
+++ test/other.c
@@ -9,16 +9,42 @@
 
 #include "../riggerd/lock.h"
 
-static void lock_file(void **state) {
+static void lock_file_call_fn(void **state) {
     lock_override("/tmp/dnssec0123456789", 21);
     lock_acquire();
     lock_release();
     (void) state; /* unused */
 }
 
+static void lock_file_check_file_presence(void **state) {
+    const char *name = "/tmp/dnssec0002";
+    lock_override(name, 15);
+    lock_acquire();
+    assert_true(access(name, F_OK) == 0);
+    lock_release();
+    (void) state; /* unused */
+}
+
+static void lock_file_check_file_permissions(void **state) {
+    const char *name = "/tmp/dnssec0002";
+    lock_override(name, 15);
+    lock_acquire();
+    assert_true(access(name, R_OK) == 0);
+    assert_true(access(name, W_OK) == 0);
+    assert_true(access(name, X_OK) == -1);
+    // TODO: check that the file is indeed locked
+    lock_release();
+    assert_true(access(name, R_OK) == 0);
+    assert_true(access(name, W_OK) == 0);
+    assert_true(access(name, X_OK) == -1);
+    (void) state; /* unused */
+}
+
 int main() {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(lock_file)
+        cmocka_unit_test(lock_file_call_fn),
+        cmocka_unit_test(lock_file_check_file_presence),
+        cmocka_unit_test(lock_file_check_file_permissions)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
