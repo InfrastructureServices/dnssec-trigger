fc8d5c1cdd51091df921c4ac391e61b05e61e68e
Martin Sehnoutka <msehnout@redhat.com>
2018-03-05 16:43:57 +0100

    implement storage creation

--- riggerd/store.c
+++ riggerd/store.c
@@ -2,6 +2,10 @@
 #include "store.h"
 
 struct store store_init(const char *dir, const char *full_path, const char *full_path_tmp) {
-    struct store s;
+    struct store s = {
+        .dir = dir,
+        .path = full_path,
+        .path_tmp = full_path_tmp
+    };
     return s;
 }
--- riggerd/store.h
+++ riggerd/store.h
@@ -11,11 +11,11 @@
  * this module. */
 #define STORE_BASE_DIR "/var/run/dnssec-trigger"
 /* Concatenate file name with the base directory. */
-#define STORE_PATH(NAME) (STORE_BASE_DIR NAME)
+#define STORE_PATH(NAME) (STORE_BASE_DIR "/" NAME)
 /* Concatenate file name with the base directory and append ".tmp"
  * to the path. As the name suggests this file will be stored
  * temporarily and eventually it will replace the normal file. */
-#define STORE_PATH_TMP(NAME) (STORE_BASE_DIR NAME ".tmp")
+#define STORE_PATH_TMP(NAME) (STORE_BASE_DIR "/" NAME ".tmp")
 
 struct store {
     const char *dir;
